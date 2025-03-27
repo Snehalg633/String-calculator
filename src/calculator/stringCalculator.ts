1b0dd97a991bdb4893880ed98bcb09aac2b2995f
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2025-03-27 17:01:00 +0530

    feat: implemented delimiter extraction and replacement

--- src/calculator/stringCalculator.ts
+++ src/calculator/stringCalculator.ts
@@ -1,35 +1,41 @@
 export function addString(numbers: string): number {
-//   if (numbers === " ") return 0;
+  //   if (numbers === " ") return 0;
 
-//   const inputWithCommas = numbers.replace(/\n/g, ",");
-//   const numberArr = inputWithCommas.split(",");
+  //   const inputWithCommas = numbers.replace(/\n/g, ",");
+  //   const numberArr = inputWithCommas.split(",");
 
-//   let sum = 0;
-//   for (let i = 0; i < numberArr.length; i++) {
-//     sum += Number(numberArr[i]);
-//   }
-//   return sum;
+  //   let sum = 0;
+  //   for (let i = 0; i < numberArr.length; i++) {
+  //     sum += Number(numberArr[i]);
+  //   }
+  //   return sum;
 
   function parseNumbers(str: string, delimiter: string): number[] {
     const numstrings = str.split(delimiter);
 
     const numberArr: number[] = [];
 
-    for(let i=0; i< numstrings.length; i++){
-        const num = Number(numstrings[i])
-        if(!isNaN(num)){
-            numberArr.push(num);
-        }
+    for (let i = 0; i < numstrings.length; i++) {
+      const num = Number(numstrings[i]);
+      if (!isNaN(num)) {
+        numberArr.push(num);
+      }
     }
     return numberArr;
   }
 
-  let newNumbers: number[]=[];
+  let newNumbers: number[] = [];
 
-  if(numbers.startsWith("//")){
+  if (numbers.startsWith("//")) {
     const delimiterEndIndex =
-    numbers.indexOf("\n") === -1
-      ? numbers.indexOf("\\n") - 1
-      : numbers.indexOf("\n");
+      numbers.indexOf("\n") === -1
+        ? numbers.indexOf("\\n") - 1
+        : numbers.indexOf("\n");
+
+    const delimiter = numbers.substring(2, delimiterEndIndex);
+    const numbersStr = numbers.substring(delimiterEndIndex + 1);
+
+    const inputWithCommas = numbersStr.replace(new RegExp(delimiter, "g"), ",");
+    newNumbers = parseNumbers(inputWithCommas.replace(/\\n/g, "\n"), ",");
   }
 }
