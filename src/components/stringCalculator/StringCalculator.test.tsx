f4ef77f256b8845a9fe3d8aecac7a292055a1f21
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2025-03-27 17:40:56 +0530

    test: added failing test case for button

--- src/components/stringCalculator/StringCalculator.test.tsx
+++ src/components/stringCalculator/StringCalculator.test.tsx
@@ -8,3 +8,10 @@ test("should render input box", () => {
   const inputBox = screen.getByTestId("numbers-input");
   expect(inputBox).toBeInTheDocument();
 });
+
+test("should render calculate button", () => {
+  render(<StringCalculator />);
+
+  const button = screen.getByText("Calculate");
+  expect(button).toBeInTheDocument();
+});
