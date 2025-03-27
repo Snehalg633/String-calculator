16469668ae7dc28b8863b5fdc5f69c03aa3f2cb8
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2025-03-27 16:55:15 +0530

    feat: implement parseNumbers function to split input and convert to numbers

--- src/calculator/stringCalculator.ts
+++ src/calculator/stringCalculator.ts
@@ -1,12 +1,26 @@
 export function addString(numbers: string): number {
-  if (numbers === " ") return 0;
+//   if (numbers === " ") return 0;
 
-  const inputWithCommas = numbers.replace(/\n/g, ",");
-  const numberArr = inputWithCommas.split(",");
+//   const inputWithCommas = numbers.replace(/\n/g, ",");
+//   const numberArr = inputWithCommas.split(",");
 
-  let sum = 0;
-  for (let i = 0; i < numberArr.length; i++) {
-    sum += Number(numberArr[i]);
+//   let sum = 0;
+//   for (let i = 0; i < numberArr.length; i++) {
+//     sum += Number(numberArr[i]);
+//   }
+//   return sum;
+
+  function parseNumbers(str: string, delimiter: string): number[] {
+    const numstrings = str.split(delimiter);
+
+    const numberArr: number[] = [];
+
+    for(let i=0; i< numstrings.length; i++){
+        const num = Number(numstrings[i])
+        if(!isNaN(num)){
+            numberArr.push(num);
+        }
+    }
+    return numberArr;
   }
-  return sum;
 }
