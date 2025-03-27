77c104f6a965b8a1bbb88b15dcbb1e677bc3039b
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2025-03-27 17:53:26 +0530

    test: added test case for display result when cliking the button

--- src/components/stringCalculator/StringCalculator.test.tsx
+++ src/components/stringCalculator/StringCalculator.test.tsx
@@ -15,3 +15,15 @@ test("should render calculate button", () => {
   const button = screen.getByText("Calculate");
   expect(button).toBeInTheDocument();
 });
+
+test("should displays result when clicking the calculate button", () => {
+  render(<StringCalculator />);
+
+  const inputBox = screen.getByTestId("numbers-input");
+  const button = screen.getByText("Calculate");
+
+  fireEvent.change(inputBox, { target: { value: "1,2,3" } });
+  fireEvent.click(button);
+
+  expect(screen.getByText("Result: 6")).toBeInTheDocument();
+});
