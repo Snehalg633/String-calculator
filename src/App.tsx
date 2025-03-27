c176e98d816bde6553408d395b33eba2b18237b0
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2025-03-27 17:37:14 +0530

    feat: added StringCalculator component in App.tsx to test the  UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import StringCalculator from "./components/stringCalculator/StringCalculator";
 
 const App = () => {
   return (
     <div>
       <h1>String Calculator</h1>
+      <StringCalculator />
     </div>
   );
 };
