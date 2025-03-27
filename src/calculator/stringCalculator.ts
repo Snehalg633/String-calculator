ed53bde85f2e5d2c7264cc5cce8f765431d1346f
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2025-03-27 16:44:01 +0530

    feat: handled new line character as separator

--- src/calculator/stringCalculator.ts
+++ src/calculator/stringCalculator.ts
@@ -1,9 +1,12 @@
 export function addString(numbers: string): number {
   if (numbers === " ") return 0;
 
+  const inputWithCommas = numbers.replace(/\n/g, ",");
+  const numberArr = inputWithCommas.split(",");
+
   let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += parseInt(numbers);
+  for (let i = 0; i < numberArr.length; i++) {
+    sum += Number(numberArr[i]);
   }
   return sum;
 }
