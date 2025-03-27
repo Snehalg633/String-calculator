91d2a4a18ee2ce10ae4fc55142f041f3a3934a3f
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2025-03-27 17:35:56 +0530

    feat: added input box

---
+++ src/components/stringCalculator/StringCalculator.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const StringCalculator = () => {
+  return (
+    <div>
+      <input
+        data-testid="numbers-input"
+        type="text"
+        placeholder="Enter numbers..."
+      />
+    </div>
+  );
+};
+
+export default StringCalculator;
