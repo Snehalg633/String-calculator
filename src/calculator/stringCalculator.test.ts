d56f1b22cb29d75d56b243184f243a0c59a55e07
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2025-03-27 15:12:41 +0530

    test: added failing test for single number input

--- src/calculator/stringCalculator.test.ts
+++ src/calculator/stringCalculator.test.ts
@@ -3,3 +3,7 @@ import { addString } from "./stringCalculator";
 test("should return 0 for empty string", () => {
   expect(addString(" ")).toBe(0);
 });
+
+test("should return number itself for single number input", () => {
+  expect(addString("1")).toBe(1);
+});
