489126b092c8d03c1231a459843a0da1cca664a8
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2025-03-27 18:13:55 +0530

    test:  addd test cases for new line as delimiter & different delimiter

--- src/components/stringCalculator/StringCalculator.test.tsx
+++ src/components/stringCalculator/StringCalculator.test.tsx
@@ -22,8 +22,24 @@ test("should displays result when clicking the calculate button", () => {
   const inputBox = screen.getByTestId("numbers-input");
   const button = screen.getByText("Calculate");
 
-  fireEvent.change(inputBox, { target: { value: "1,2,3" } });
+  fireEvent.change(inputBox, { target: { value: "1,2" } });
+  fireEvent.click(button);
+
+  expect(screen.getByText("Result: 3")).toBeInTheDocument();
+});
+
+test("should handle different inputs correctlly", () => {
+  render(<StringCalculator />);
+  const inputBox = screen.getByTestId("numbers-input");
+  const button = screen.getByText("Calculate");
+
+  fireEvent.change(inputBox, { target: { value: "1\n2,3" } });
   fireEvent.click(button);
 
   expect(screen.getByText("Result: 6")).toBeInTheDocument();
+
+  fireEvent.change(inputBox, { target: { value: "//|\n3|4|5" } });
+  fireEvent.click(button);
+
+  expect(screen.getByText("Result: 12")).toBeInTheDocument();
 });
